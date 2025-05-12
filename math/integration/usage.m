1b4452501d910d439aff2da468f0dd36269ec35c
Nicolas Vyčas Nery <vycasnicolas@gmail.com>
2025-05-12 15:20:00 -0300

    Update usage.m

--- math/integration/usage.m
+++ math/integration/usage.m
@@ -8,3 +8,15 @@ I_simp = simpsons_rule(f, a, b, n);
 
 fprintf('Trapezoidal Rule: %.8f\n', I_trap);
 fprintf('Simpson''s Rule : %.8f\n', I_simp);
+
+
+%
+f = @(x) exp(x);
+a = 0
+b = 1
+h = (a-b) / 5
+x = a:h:b
+y = f(x)
+Isimpson = h/3 * (y(1) + 4 * sum(y(2:2:end-1)) + 2 * sum(y(3:2:end-2)) + y(end));
+ITrapzoid = h * 0.5 * (y(1) + 2*sum(y(2:end-1)) + y(end));
+%
